297232e2908a808da73807f824101278cee3a40a
Yurii Serediuk <yserediuk@n-ix.com>
2017-03-09 17:48:16 +0200

    Fixed test02: join to string.

--- test/i_introduction/_2_Named_Arguments/_02_Default_And_Named_Params.kt
+++ test/i_introduction/_2_Named_Arguments/_02_Default_And_Named_Params.kt
@@ -5,7 +5,8 @@ import org.junit.Assert.assertEquals
 class _02_Default_And_Named_Params() {
 
     @org.junit.Test fun testJoinToString() {
-        assertEquals("{1, 2, 3, 42, 555}", task2(listOf(1, 2, 3, 42, 555)))
+        val listStr = listOf(1, 2, 3, 42, 555).joinToString(prefix = "{", postfix = "}")
+        assertEquals("{1, 2, 3, 42, 555}", listStr)
     }
 
 }
